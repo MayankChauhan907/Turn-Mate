8f1041a840e1a262d822f36e0a5980ec909ccd05
MayankChauhan907 <mayankdarbar907@gmail.com>
2023-01-21 20:48:09 +0530

    Make Player Turn On Mouse Click

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField] Vector3 _startPos;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] float _speed = 10f;
+    Quaternion _headinRotation;
+    bool _isDirectionSet = false;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
 
+            if (Physics.Raycast(ray, out hitInfo, _layerMask))
+            {
+                _startPos = hitInfo.point;
+                _isDirectionSet = true;
+                /*GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Cube.transform.position = hitInfo.point;
+                Cube.transform.rotation = hitInfo.transform.rotation;*/
+            }
+
+        }
+        if (_isDirectionSet)
+        {
+            rotateToDirection(_startPos, transform.position);
+        }
+    }
+
+    void rotateToDirection(Vector3 oldPos, Vector3 newPos)
+    {
+        var Direction = oldPos - newPos;
+        Quaternion LookAtRotation = Quaternion.LookRotation(Direction);
+        Quaternion LookAtRotationOnly_Y = Quaternion.Euler(transform.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y - 90, transform.rotation.eulerAngles.z);
+        //transform.rotation = LookAtRotationOnly_Y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, LookAtRotationOnly_Y, Time.deltaTime * _speed);
     }
 }
